afefa521b553f67dd5630948d0a0dcf6829cef09
hajixhayjhay <akandehajarat@gmail.com>
2025-09-03 16:43:38 +0800

    updated test file

--- src/test/java/com/studentapp/NumberGuessServletTest.java
+++ src/test/java/com/studentapp/NumberGuessServletTest.java
@@ -1,54 +1,30 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<project xmlns="http://maven.apache.org/POM/4.0.0"
-         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-    <modelVersion>4.0.0</modelVersion>
+package com.studentapp;
 
-    <groupId>com.studentapp</groupId>
-    <artifactId>NumberGuessGame</artifactId>
-    <version>1.0-SNAPSHOT</version>
-    <packaging>war</packaging>
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
-    <name>Number Guessing Game</name>
-    <url>http://maven.apache.org</url>
+public class NumberGuessServletTest {
+    private NumberGuessServlet servlet;
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private StringWriter responseWriter;
 
-    <dependencies>
-        <!-- Servlet API -->
-        <dependency>
-            <groupId>javax.servlet</groupId>
-            <artifactId>javax.servlet-api</artifactId>
-            <version>4.0.1</version>
-            <scope>provided</scope>
-        </dependency>
+    @Before
+    public void setUp() throws Exception {
+        servlet = new NumberGuessServlet();
+        servlet.init();
+        request = Mockito.mock(HttpServletRequest.class);
+        response = Mockito.mock(HttpServletResponse.class);
+        responseWriter = new StringWriter();
+        Mockito.when(response.getWriter()).thenReturn(new PrintWriter(responseWriter));
+    }
 
-        <!-- JUnit for Testing -->
-        <dependency>
-            <groupId>junit</groupId>
-            <artifactId>junit</artifactId>
-            <version>4.13.2</version>
-            <scope>test</scope>
-        </dependency>
-    </dependencies>
-
-    <build>
-        <plugins>
-            <!-- Compiler Plugin -->
-            <plugin>
-                <groupId>org.apache.maven.plugins</groupId>
-                <artifactId>maven-compiler-plugin</artifactId>
-                <version>3.8.1</version>
-                <configuration>
-                    <source>11</source>
-                    <target>11</target>
-                </configuration>
-            </plugin>
-
-            <!-- WAR Plugin -->
-            <plugin>
-                <groupId>org.apache.maven.plugins</groupId>
-                <artifactId>maven-war-plugin</artifactId>
-                <version>3.3.2</version>
-            </plugin>
-        </plugins>
-    </build>
-</project>
+    @Test
+    public void testGuessTooLow() throws Exception {
+        Mockit
